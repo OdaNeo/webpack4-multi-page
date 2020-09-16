22135d9c48eaf67a7b77af873e865fd3bd412502
Oda-T <46777459+Oda-T@users.noreply.github.com>
2020-09-16 21:11:01 +0800

    add postcss px2rem

--- push.sh
+++ push.sh
@@ -6,6 +6,6 @@ git add -A
 
 git commit -m 'add postcss px2rem'
 
-git push git@github.com:Oda-T/webpack4-multi-page.git master:master
+git push -f git@github.com:Oda-T/webpack4-multi-page.git master:master
 
 cd -
