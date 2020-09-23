4d812829099e38a290c4e959a97eb73366a9adf3
Oda-T <46777459+Oda-T@users.noreply.github.com>
2020-09-23 18:59:24 +0800

    publicPath

--- webpack.dev.js
+++ webpack.dev.js
@@ -19,7 +19,7 @@ module.exports = merge(common, {
   devtool: 'eval-cheap-module-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
-    port: 7864,
+    port: 7863,
     open: true,
     hot: true
   },
