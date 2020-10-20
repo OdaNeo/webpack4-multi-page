1502e3b094a754c5d2dcecb6d6e1b51fae9a3c26
Oda-T <oda444567@hotmail.com>
2020-10-20 14:58:00 +0800

    add raw-loader  css tree shaking

--- push.sh
+++ push.sh
@@ -6,6 +6,6 @@ git add -A
 
 git commit -m 'add raw-loader  css tree shaking'
 
-git push git@github.com:Oda-T/webpack4-multi-page.git master:master
+git push -f git@github.com:Oda-T/webpack4-multi-page.git master:master
 
 cd -
