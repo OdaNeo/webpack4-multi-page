6a78e61b19ecb78bfc070dacc7565e62f10578cf
OdaNeo <odaneo@outlook.com>
2021-01-01 23:39:46 +0800

    add eslint parser

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,3 @@
-set -e
-
-npm run build:gh
-
-cd dist
-
-git init
-
 git add -A
-
-git commit -m 'deploy'
-
-git push -f git@github.com:Oda-T/webpack4-multi-page.git master:gh-pages
-
-cd..
+git commit -m 'add eslint parser'
+git push origin master
