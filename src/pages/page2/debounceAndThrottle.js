6b6ee693e2f29e4fbfb41f296254ef7d76356ee7
odaneo <odaneo@outlook.com>
2024-08-20 23:08:27 +0900

    feat(debounce): debounce and Throttle

---
+++ src/pages/page2/debounceAndThrottle.js
@@ -0,0 +1,39 @@
+let count = 1
+const container = document.getElementById('container')
+
+function getUserAction(e) {
+  console.log(this)
+  console.log(e)
+  container.innerHTML = count++
+}
+
+function debounce(func, wait) {
+  let timeout
+  return function () {
+    func = func.bind(this)
+    const args = arguments
+
+    clearTimeout(timeout)
+
+    timeout = setTimeout(function () {
+      func(args)
+    }, wait)
+  }
+}
+
+function throttle(func, wait) {
+  let previous = 0
+
+  return function () {
+    const now = +new Date()
+    func = func.bind(this)
+    const args = arguments
+
+    if (now - previous > wait) {
+      func(args)
+      previous = now
+    }
+  }
+}
+
+container.onmousemove = throttle(getUserAction, 1000)
---
+++ src/pages/page2/index.js
@@ -0,0 +1,6 @@
+import '@/styles/page2.css' // import css
+import './debounceAndThrottle'
+
+if (module.hot) {
+  module.hot.accept()
+}
